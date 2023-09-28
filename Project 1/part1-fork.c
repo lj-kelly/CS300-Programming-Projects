2528cc02b80bcedfef26b96da1e07552e6da6926
Liam Kelly <119522899+lj-kelly@users.noreply.github.com>
2023-09-28 13:59:45 -0500

    Update part1-fork.c

--- Project 1/part1-fork.c
+++ Project 1/part1-fork.c
@@ -7,6 +7,7 @@ int main(int argc, char* argv[]){
 
     printf("hello world (pid:%d)\n", (int) getpid());
     int x = 100;
+    printf("x value is:%d\n", x);
     int rc = fork();
 
     if (rc < 0) { // fork failed; exit
@@ -14,11 +15,15 @@ int main(int argc, char* argv[]){
         exit(1);
     }
     else if (rc == 0) { // child (new process)
+        x = 101;
         printf("hello, I am child (pid:%d)\n", (int) getpid());
+        printf("x value is:%d\n", x);
     }
     else { // parent goes down this path (main)
+        x = 102;
         int rc_wait = wait(NULL);
         printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
+        printf("x value is:%d\n", x);
     }
     return 0;
 }
