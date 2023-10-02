0f5727391d2790cf49ea457f0de77270063fc205
Liam Kelly <119522899+lj-kelly@users.noreply.github.com>
2023-10-02 14:56:27 -0500

    fix

--- Project 1/part2-exec.c
+++ Project 1/part2-exec.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[]) {
   } 
   else if (rc == 0) { // child (new process)
     printf("hello, I am child (pid:%d)\n", (int) getpid());
-    //execl("/bin/ls", "ls", "-1", NULL); // execl call (part 3a)
-    execle("/bin/ls", "ls", "-1", NULL, NULL); // execle call (part 3b)
-    //execlp("ls", "ls", "-1", NULL); // execlp call  (part 3c)
-    //execv("/bin/ls", argv); // execv call (part 3d)
-    //execvp("ls", argv); // execvp call (part 3e)
-    //execvpe("ls", argv, NULL); // execvpe call (part 3f)
+    //execl("/bin/ls", "ls", "-1", NULL); // execl call (part 2a)
+    execle("/bin/ls", "ls", "-1", NULL, NULL); // execle call (part 2b)
+    //execlp("ls", "ls", "-1", NULL); // execlp call  (part 2c)
+    //execv("/bin/ls", argv); // execv call (part 2d)
+    //execvp("ls", argv); // execvp call (part 2e)
+    //execvpe("ls", argv, NULL); // execvpe call (part 2f)
     printf("this shouldn't print out");
   }
   else { // parent goes down this path (main)
