e4bf3f35fc782c5bc2cdf9d75cd54034e95721ce
Liam Kelly <119522899+lj-kelly@users.noreply.github.com>
2023-10-02 14:52:56 -0500

    more changes

--- Project 1/part2-exec.c
+++ Project 1/part2-exec.c
@@ -13,7 +13,12 @@ int main(int argc, char *argv[]) {
   } 
   else if (rc == 0) { // child (new process)
     printf("hello, I am child (pid:%d)\n", (int) getpid());
-    execl("/bin/ls", "ls", "-1", NULL); // runs word count
+    //execl("/bin/ls", "ls", "-1", NULL); // execl call (part 3a)
+    execle("/bin/ls", "ls", "-1", NULL, NULL); // execle call (part 3b)
+    //execlp("ls", "ls", "-1", NULL); // execlp call  (part 3c)
+    //execv("/bin/ls", argv); // execv call (part 3d)
+    //execvp("ls", argv); // execvp call (part 3e)
+    //execvpe("ls", argv, NULL); // execvpe call (part 3f)
     printf("this shouldn't print out");
   }
   else { // parent goes down this path (main)
