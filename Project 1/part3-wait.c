4c367f09935366eae35c68d84ad8923e2fa1f002
Liam Kelly <119522899+lj-kelly@users.noreply.github.com>
2023-10-04 15:51:52 -0500

    made changes for readability

--- Project 1/part3-wait.c
+++ Project 1/part3-wait.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
+  printf("waitpid in parent call\n");
   printf("hello world (pid:%d)\n", (int) getpid());
   int rc = fork();
   if (rc < 0) { // fork failed; exit
@@ -12,16 +13,22 @@ int main(int argc, char *argv[]) {
   } 
   else if (rc == 0) { // child (new process)
     
-    int rc_wait = wait(NULL); // (part 3a)
-    //int rc_wait = waitpid(rc, NULL, 0); (part 3b)
-    printf("hello, I am child of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
+    //int rc_wait = wait(NULL); // (part of child wait call)
+    //int rc_wait = waitpid(rc, NULL, 0); //(part of child waitpid call)
+    //printf("hello, I am child of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid()); //(child wait call)
+
+
+    printf("hello, I am child (pid:%d)\n", (int) getpid()); //(child pid call)
     
     
   } 
   else { // parent goes down this path (main)
-    //int rc_wait = wait(NULL); (part of parent wait call)
-    printf("hello, I am parent (pid:%d)\n", (int) getpid());
-    //printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid()); (parent wait call)
+    //int rc_wait = wait(NULL); //(part of parent wait call)
+    int rc_wait = waitpid(rc, NULL, 0); //(part of parent waitpid call)
+    printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid()); //(parent wait call)
+
+    //printf("hello, I am parent (pid:%d)\n", (int) getpid()); //(parent pid call)
   }
+
   return 0;
 }
