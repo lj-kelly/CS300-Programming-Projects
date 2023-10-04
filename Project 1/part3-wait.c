c8422d035315308940eb7113c32be44cb2db3cf6
Liam Kelly <119522899+lj-kelly@users.noreply.github.com>
2023-10-04 09:59:42 -0500

    updates

--- Project 1/part3-wait.c
+++ Project 1/part3-wait.c
@@ -12,16 +12,16 @@ int main(int argc, char *argv[]) {
   } 
   else if (rc == 0) { // child (new process)
     
-    int rc_wait = wait(NULL);
-    //int rc_wait = waitpid(rc, NULL, 0);
+    int rc_wait = wait(NULL); // (part 3a)
+    //int rc_wait = waitpid(rc, NULL, 0); (part 3b)
     printf("hello, I am child of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
     
     
   } 
   else { // parent goes down this path (main)
-
+    //int rc_wait = wait(NULL); (part of parent wait call)
     printf("hello, I am parent (pid:%d)\n", (int) getpid());
-    
+    //printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid()); (parent wait call)
   }
   return 0;
 }
